9c16ea89c71239ff9351af2588f13254a5b37e08
Dzhanybek Zakiriiaev <dzhanybek.zakiriiaev@gmail.com>
2024-02-13 16:01:57 -0500

    cahnged tests for setWidth and setLength and added annotations

--- REQ_002/REQ_002.cpp
+++ REQ_002/REQ_002.cpp
@@ -13,18 +13,21 @@ namespace REQ002
 	public:
 		TEST_METHOD(widthFunctionalityGeneral)
 		{
-			int width = 1;
+			//This is testing setWidth general functionality
+			int width = 17;
 			setWidth(12, &width);
 			Assert::AreEqual(12, width);
 		}
-		TEST_METHOD(widthFunctionalityMax)
+		TEST_METHOD(widthFunctionalityMin)
 		{
+			//This is testing setWidth max values functionality
 			int width = 1;
 			setWidth(1, &width);
 			Assert::AreEqual(1, width);
 		}
-		TEST_METHOD(widthFunctionalityMin)
+		TEST_METHOD(widthFunctionalityMax)
 		{
+			//This is testing setWidth min values functionality
 			int width = 0;
 			setWidth(99, &width);
 			Assert::AreEqual(99, width);
@@ -32,18 +35,21 @@ namespace REQ002
 
 		TEST_METHOD(lengthFunctionalityGeneral)
 		{
-			int length = 1;
+			//This is testing setLength general functionality
+			int length = 57;
 			setLength(43, &length);
 			Assert::AreEqual(43, length);
 		}
-		TEST_METHOD(lengthFunctionalityMax)
+		TEST_METHOD(lengthFunctionalityMin)
 		{
+			//This is testing setLength general min values functionality
 			int length = 1;
 			setLength(1, &length);
 			Assert::AreEqual(1, length);
 		}
-		TEST_METHOD(lengthFunctionalityMin)
+		TEST_METHOD(lengthFunctionalityMax)
 		{
+			//This is testing setLength general max values functionality
 			int length = 99;
 			setLength(1, &length);
 			Assert::AreEqual(1, length);
